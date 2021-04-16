d143d8fc4be41c6c061c2c1ee4ae54b6f3a86933
alexastro80 <astroud@harding.edu>
2021-04-16 15:27:04 -0400

    Fixed obdServer, turned DEBUG Mode off!

--- lib/scripts/OBD/obdServer.py
+++ lib/scripts/OBD/obdServer.py
@@ -7,17 +7,20 @@ import os
 import socket
 #import pint
 import sys
+import struct
 #dirpath = os.getcwd()
 #obdPath = dirpath + "/../../../python-OBD/obd/"
 #print(obdPath)
 #sys.path.append(obdPath)
 import obd
 from obd import OBDStatus
-
+from obd import OBDResponse
 import pint
+import math
 BUFF_SIZE = 9
-DEBUG = True
+DEBUG = False
 UDP_IP = "127.0.0.1"
+
 
 parser = argparse.ArgumentParser(description='Interface with OBD module')
 ureg = pint.UnitRegistry()
@@ -27,6 +30,7 @@ parser.add_argument('svrPort',
                     help='the local port for server communication')
 
 def main(args):
+    t = 0.0
     #setup server socket
     udp_port = args.svrPort
     svrSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -58,44 +62,65 @@ def main(args):
         print("Connecting. . .")
         if (not DEBUG):
             connection = obd.OBD(fast=False, timeout=30)
+        else:
+            t = 0.0
         print(connection.status())
         #sleep(2)
     print("Connected!")
     if (not DEBUG):
         response = connection.query(obd.commands.RPM)
     else:
-        response = 3000 * ureg.meter
+        response = OBDResponse()
+        response.value = 3000 * ureg.meter
     if not response.is_null():
         print(response.value)
     while ((not response.is_null()) and response.value.magnitude > 300 or DEBUG):
         # Get OBD Code from application
         recvBuf = svrSock.recvfrom(BUFF_SIZE)
         packet = recvBuf[0]
-        if (packet is not None and len(packet) > 0):
+        if (packet is not None and len(packet) > 1):
             # packet has been received
             sendAddress = recvBuf[1]
-            packetType = int(packet[0])
+            
+            packetType = ord(packet[0])
             if (packetType == 2):
-                codeNum = int(packet[1])
+                codeNum = ord(packet[1])
                 if (not DEBUG):
                     response = connection.command[1][codeNum]
                 else:
-                    response = 15.56 * ureg.meter
+                    response = OBDResponse()
+                    response.value = (15.56 * ureg.meter)
                 if (not response.is_null):
                     if (len(response) == 1):
                         #Query was for a specific value
                         response.to_base_units()
                         responseVal = double(response.value.magnitude)
-                        packet = bytearray([1,struct.pack('d', responseVal)])
-                        svrSock.sendto(packet)
+
+                        packet = struct.pack('Bid', 1, codeNum, responseVal)
+                        print("Sending " + str(responseVal))
+                        print(packet)
+                        svrSock.sendto(packet, sendAddress)
+                elif (DEBUG):
+                    t = t + 0.05
+                    responseVal = (50.25/2.0)*(math.sin(2*math.pi*0.02*t) + 1)
+                    packet = struct.pack('bbd', 1, 0x0D, responseVal)
+                    print("Sending " + str(responseVal))
+                    print(packet)
+                    svrSock.sendto(packet,sendAddress)
                 else:
-                    packet = bytearray([1,struct.pack('d', 0.0)])
-                    svrSock.sendto(packet)
+                    responseVal = 0.0
+                    packet = struct.pack('bbd', 1,0x0D, 0.0)
+                    string = ""
+                    print("Sending " + str(responseVal))
+                    print(packet)
+                    svrSock.sendto(packet, sendAddress)
+
         #Check if Engine was shutoff
         if (not DEBUG):
             response = connection.query(obd.commands.RPM)
         else:
-           response = 3000 * ureg.meter
+            response = OBDResponse()
+            response.value = 3000 * ureg.meter
         if not response.is_null():
             print(response.value)
     print("Car turned off")
