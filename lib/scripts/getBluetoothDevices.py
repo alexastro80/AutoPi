70d5b5614102eabe92cd75e0f0a8c11690715356
alexastro80 <astroud@harding.edu>
2021-07-10 12:55:41 -0400

    Updated bluetoothDevices.py updated gitignore

--- lib/scripts/getBluetoothDevices.py
+++ lib/scripts/getBluetoothDevices.py
@@ -5,10 +5,18 @@ bus = pydbus.SystemBus()
 adapter = bus.get('org.bluez', '/org/bluez/hci0')
 manager = bus.get('org.bluez', '/')
 managedObjects = manager.GetManagedObjects()
+file1 = open("connectedBluetoothDevices.txt", "w")
+file2 = open("otherBluetoothDevices.txt", "w")
 for path in managedObjects:
     connectState = managedObjects[path].get('org.bluez.Device1', {}).get('Connected', False)
+    address = managedObjects[path].get('org.bluez.Device1', {}).get('Address')
+    name = managedObjects[path].get('org.bluez.Device1', {}).get('Name')
+    string = str(name) + " {" + str(address) + "}"
+    print(string)
     if connectState:
-        address = managedObjects[path].get('org.bluez.Device1', {}).get('Address')
-        name = managedObjects[path].get('org.bluez.Device1', {}).get('Name')
-        print(f'Device {name} [{address}] is connected')
-            
+        file1.write(string)
+    else:
+        file2.write(string)
+        
+file1.close()
+file2.close()
